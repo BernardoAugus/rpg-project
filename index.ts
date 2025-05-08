f6a9a374ba4c853fb459ecb8c31bbfa1e80cc2af
BernardoAugus <bernardo.11salgado@hotmail.com>
2025-05-08 00:00:47 -0300

    feat: configura bootstrap inicial do Express e conexão TypeORM

---
+++ index.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import express from 'express';
+import { DataSource } from 'typeorm';
+import { AppDataSource } from './infrastructure/db/data-source';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('📦 Banco de dados conectado');
+
+    app.listen(port, () => {
+      console.log(`🚀 Servidor rodando em http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('❌ Erro ao conectar no banco de dados', error);
+  });
